9f94b5d74cb6f8547e6c894b4acfb107b8ffc303
Takeshi <a86487817@gmail.com>
2014-11-22 15:20:34 +0800

    add broad cast, tell, who features

--- client_node.c
+++ client_node.c
@@ -1,6 +1,8 @@
 #include "client_node.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #include "cmd_node.h"
 
 client_node_t* create_client_node(int client_sc_fd, char ip[16], int port) {
@@ -8,7 +10,7 @@ client_node_t* create_client_node(int client_sc_fd, char ip[16], int port) {
     client_node_t* new_client_node = malloc(sizeof(client_node_t));
 
     new_client_node->id = -1;
-    new_client_node->client_sc_fd;
+    new_client_node->client_sc_fd = client_sc_fd;
     bzero(new_client_node->name, 20);
     strncpy(new_client_node->name, "(no name)", 10);
     strncpy(new_client_node->ip, ip, 16);
@@ -26,7 +28,7 @@ client_node_t* create_client_node(int client_sc_fd, char ip[16], int port) {
 
 int insert_to_client_list(client_node_t* new_client_node) {
     int c;
-    for(c=0; c<30; c++) {
+    for(c=1; c<=30; c++) {
         if(client_list[c] == NULL) {
             new_client_node->id = c;
             client_list[c] = new_client_node;
@@ -38,10 +40,86 @@ int insert_to_client_list(client_node_t* new_client_node) {
 
 client_node_t* find_client_node(int fd) {
     int c;
-    for(c=0; c<30; c++) {
+    for(c=1; c<=30; c++) {
         if(client_list[c] != NULL && client_list[c]->client_sc_fd == fd) {
             return client_list[c];
         }
     }
     return NULL;
+}
+
+void broad_cast(client_node_t* current_client, char* message) {
+    int c;
+    for(c=1; c<=30; c++) {
+        if(client_list[c] != NULL) {
+            dup2(client_list[c]->client_sc_fd, 1);
+            printf("%s", message);
+            fflush(stdout);
+        }
+    }
+    dup2(current_client->client_sc_fd, 1);
+}
+
+
+void set_env_to_client_node(client_node_t* client, char* name, char* val) {
+    int c;
+    for(c=0; c<client->num_env; c++) {
+        if(strcmp(client->env[c], name) == 0) {
+            strcpy(client->env_val[c], val);
+            return;
+        }
+    }
+    strcpy(client->env[client->num_env], name);
+    strcpy(client->env_val[client->num_env], name);
+    client->num_env++;
+}
+
+void remove_client_node(client_node_t* client) {
+    int c;
+    for(c=1; c<=30; c++) {
+        if(client_list[c] == client) {
+            client_list[c] = NULL;
+        }
+    }
+}
+
+
+int tell(client_node_t* current_client, char* client_id_str, char* message) {
+    int c;
+    int client_id = atoi(client_id_str);
+    if(client_list[client_id] == NULL || client_id < 0 || client_id >= 30) {
+        return -1;
+    } else {
+        dup2(client_list[client_id]->client_sc_fd, 1);
+        printf("*** %s told you ***: %s\r\n", current_client->name, message);
+        fflush(stdout);
+        dup2(current_client->client_sc_fd, 1);
+        return 0;
+    }
+}
+
+int who(client_node_t* current_client) {
+    int c;
+    printf("<ID>\t<nickname>\t<IP/port>\t<indicate me>\r\n");
+    for(c=1; c<=30; c++) {
+        client_node_t* t_client = client_list[c];
+        if(t_client != NULL) {
+            printf("%d\t%s\t%s/%d", c, t_client->name, t_client->ip, t_client->port);
+            if(client_list[c] == current_client) {
+                printf("\t<-me");
+            }
+            printf("\r\n");
+            fflush(stdout);
+        }
+    }
+}
+
+int check_name_exist(char* name) {
+    int c;
+    for(c=1; c<=30; c++) {
+        if(client_list[c] != NULL && strcmp(client_list[c]->name, name) == 0) {
+            return 1;
+        }
+    }
+    return 0;
 }
--- client_node.h
+++ client_node.h
@@ -23,8 +23,14 @@ typedef struct client_node client_node_t;
 client_node_t* create_client_node(int client_socket_fd, char ip[16], int port);
 int insert_to_client_list(client_node_t* new_client_node);
 client_node_t* find_client_node(int fd);
+void broad_cast(client_node_t* current_client, char* message);
+int tell(client_node_t* current_client, char* client_id_str, char* message);
+void set_env_to_client_node(client_node_t* client, char* name, char* val);
+void remove_client_node(client_node_t* client);
+int who(client_node_t* current_client);
+int check_name_exist(char* name);
 
 // global variable
-client_node_t* client_list[31];
+client_node_t* client_list[32];
 
 #endif
